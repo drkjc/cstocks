5d6ecb3943faee5afb8d73bbcb520dcb0ca0000f
drkjc <drkjc@protonmail.com>
2020-05-16 16:47:22 -0700

    stock model returns stock json from api and parses correctly

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,10 @@
 class StocksController < ApplicationController
 
+    def show
+        @stock = Stock.find(params[:id])
+        @prices = Stock.find_from_api(@stock.symbol)
+    end
+
     def industries 
         @industries = Stock.group_by_industry
     end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,17 @@
 class Stock < ApplicationRecord
 
+    def self.alter_api_keys(response)
+        stock = {}
+        response.each do |k, v|
+            if v.include?('.')
+                stock[k.sub(/\d.. /, '')] = v.slice(/\-\d.\d\d|\d\d.\d\d|\d.\d\d/)
+            else
+                stock[k.sub(/\d.. /, '')] = v
+            end 
+        end
+        stock
+    end
+
     def self.group_by_industry 
         industries = {}
         Stock.all.each do |stock|
@@ -12,11 +24,16 @@ class Stock < ApplicationRecord
         industries
     end
 
+    def self.find_from_api(symbol) 
+        response = Request.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{symbol}&apikey=#{ENV['av_api_key']}")
+        alter_api_keys(response["Global Quote"])
+    end
+
     def self.search(symbol)
         response = Request.get("?function=SYMBOL_SEARCH&keywords=#{symbol}&apikey=#{ENV['av_api_key']}")
         
-        @stock = Stock.new(stock_hash)
-        @stock.save
-        @stock
+        # @stock = Stock.new(stock_hash)
+        # @stock.save
+        # @stock
     end
 end
