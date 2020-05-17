e9b0b7708550ed0fbae2593892f285343d443660
drkjc <drkjc@protonmail.com>
2020-05-17 10:59:19 -0700

    Portfolio index route

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,11 @@
 class PortfoliosController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        if current_user.portfolio
+            redirect_to portfolio_path(current_user.portfolio)
+        else
+            @portfolio = Portfolio.new
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  has_one :portfolio
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def username 
+    self.email.split('@')[0].capitalize
+  end
+
 end
