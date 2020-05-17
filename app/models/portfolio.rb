4e8763f79d45848e89d1edbfeb133dd346b41678
drkjc <drkjc@protonmail.com>
2020-05-16 21:04:25 -0700

    devise gem added and installed with user model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,5 @@
 class Portfolio < ApplicationRecord
+    has_many :stocks
+    belongs_to :manager 
+    belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios
   resources :stocks
   get '/industry/id', to: 'stocks#industry', as: 'industry'
