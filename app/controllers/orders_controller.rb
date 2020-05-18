5904c8f409bda7101ebb14c8d77f67012c25e88a
drkjc <drkjc@protonmail.com>
2020-05-17 17:34:47 -0700

    user can request trade and view pending trade on portfolio show page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,20 @@
 class OrdersController < ApplicationController
 
     def create
-        raise params.inspect
+        order = Order.create(order_params)
+        order.balance = params[:order][:number_of_shares].to_i * params[:order][:share_price].to_i
+        if order.portfolio.user_id != current_user.id 
+            order.status = "success"
+            order.save 
+        else
+            order.save
+            redirect_to portfolio_path(order.portfolio_id)
+        end
+    end
+
+    private 
+
+    def order_params
+        params.require(:order).permit(:symbol, :name, :number_of_shares, :share_price, :portfolio_id, :status)
     end
 end
